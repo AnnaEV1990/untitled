8b17e3a79475820bce01467873dd57f13b05b23d
Anna Evseykina <anytta9032@yandex.ru>
2024-02-02 08:10:36 +0700

    changing variable names1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int start_balance = 200;
-        int replenishment_of_balance = 1400;
+        int startBalance = 200;
+        int replenishmentOfBalance = 1400;
 
         int bonus;
-        if (replenishment_of_balance > 1000) {
+        if (replenishmentOfBalance > 1000) {
             bonus = 1;
         } else {
             bonus = 0;
         }
 
-        System.out.println(replenishment_of_balance / 100 + start_balance + replenishment_of_balance + "Итоговая сумма на счету");
+        System.out.println(replenishmentOfBalance / 100 + startBalance + replenishmentOfBalance + "Итоговая сумма на счету");
     }
 
 }
