99b1602de1e628ec61566e93d763fe35d935aa74
Anna Evseykina <anytta9032@yandex.ru>
2024-02-03 17:12:12 +0700

    changing formula

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int startBalance = 200;
-        int replenishmentOfBalance = 1400;
+        int startBalance = 100;
+        int replenishmentOfBalance = 300;
 
         int bonus;
         if (replenishmentOfBalance > 1000) {
-            bonus = 1;
+            bonus = replenishmentOfBalance/100;
         } else {
             bonus = 0;
         }
 
-        System.out.println(replenishmentOfBalance / 100 + startBalance + replenishmentOfBalance + "Итоговая сумма на счету");
+        System.out.println(bonus + startBalance + replenishmentOfBalance + "Итоговая сумма на счету");
     }
 
 }
